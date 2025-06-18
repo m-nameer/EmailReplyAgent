d09fa55e9ce8eeb087ac9e2afc8547c913181788
Muhammad Nameer Anjum <95569188+m-nameer@users.noreply.github.com>
2025-06-18 14:22:51 +0500

    Update api.ts

--- email-agent-frontend/src/lib/api.ts
+++ email-agent-frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "http://localhost:8000"
+const API_URL = "https://2803-39-48-219-229.ngrok-free.app"
 // const API_URL = "https://d53a-124-29-232-126.ngrok-free.app"
 
 export const api = (token?: string) =>
